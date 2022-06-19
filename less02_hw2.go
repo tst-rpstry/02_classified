f1028ece752f4d3aa3fdb6abf9eebf7eed7746a4
tst-rpstry <49373540+tst-rpstry@users.noreply.github.com>
2022-06-19 14:37:55 +0600

    done

---
+++ less02_hw2.go
@@ -0,0 +1,9 @@
+// 1. create a new int array for 750 elements.
+// Fill an array with any data you want.
+
+// 2. create an empty slice
+// Fill slice with array elements from ex.1
+// Try not to use for cycle.
+
+// 3. Write a program that behaves similar to the unix wc util.
+// Read the text in moby.go and print top 20 words encountered in text.
